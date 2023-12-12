4238a387249a9c332c6e2bbd3744708ce96e29c7
Tyler McKenna <tmckenna1988@gmail.com>
2023-12-12 12:43:11 -0500

    Addition: Added the EJS rendering engine for rendering out the pages to serve to the client. Fixed: Fixed user authentication callback URI's so the user is properly routed to the auth server, can log in, and get directed to their profile page.

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const homeController = require("../controllers/home");
 
-router.get("/", homeController.getIndex);
+router.get("/", (req, res) => {
+    // This will log if the user is authenticated in the VS Code / Server terminal. Should return true of false. This will be removed for production.
+    console.log(`Is user authenticated? ${req.oidc.isAuthenticated()}`);
+
+    // If the user is authenticated; the EJS rendering engine will render the profile page. Else; it will render the index/login page.
+    req.oidc.isAuthenticated() ? res.render("profile") : res.render("index");
+});
 
 module.exports = router;
